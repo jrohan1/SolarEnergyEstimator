76087faf30452c9cd8b9ac6b4c7925b79756fd3d
jrohan1 <j.rohan1@nuigalway.ie>
2020-01-10 15:48:23 +0000

    Accelerometer data added to pitch finder screen

--- screens/PitchFinder.js
+++ screens/PitchFinder.js
@@ -1,35 +1,65 @@
 import React, { Component } from 'react'
 import { withFirebaseHOC } from '../config/Firebase'
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, Platform } from 'react-native';
 import { Camera } from 'expo-camera';
 import * as Permissions from 'expo-permissions';
+import * as ImagePicker from 'expo-image-picker';
 import { FontAwesome, Ionicons } from '@expo/vector-icons';
 import { styles } from '../stylesheets/PitchFinderStyles'
+import { Accelerometer } from "expo-sensors";
+
+Accelerometer.setUpdateInterval(200)
 
 class PitchFinder extends Component {
 
   state = {
     hasPermission: null,
     type: Camera.Constants.Type.back,
+    accelerometerData: { x: 0, y: 0, z: 0 },
+    pitch: 0
+  }
+
+  async componentWillUnmount() {
+    if(this.subscribeToAccelerometer) {
+      this.unsubscribeFromAccelerometer();
+    }
   }
 
   async componentDidMount() {
-    const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({ hasPermission: status === 'granted' });
+    this.getPermissionAsync();
+    this.subscribeToAccelerometer();
   }
 
+  getPermissionAsync = async () => {
+     // Camera roll Permission 
+    if (Platform.OS === 'ios') {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if (status !== 'granted') {
+        alert('Sorry, we need camera roll permissions to make this work!');
+      }
+    }
+      // Camera Permission
+      const { status } = await Permissions.askAsync(Permissions.CAMERA);
+      this.setState({ hasPermission: status === 'granted' });
+  }
+
+  // pickImage = async () => {
+  //   let result = await ImagePicker.launchImageLibraryAsync({
+  //     mediaTypes: ImagePicker.MediaTypeOptions.Images,
+  //   });
+  // }
+
+  subscribeToAccelerometer = () => {
+    this.accelerometerSubscription = Accelerometer.addListener(accelerometerData => this.setState({ accelerometerData })
+    );
+  };
+
+  unsubscribeFromAccelerometer = () => {
+    this.accelerometerSubscription && this.acceleroMeterSubscription.remove();
+    this.accelerometerSubscription = null;
+  };
+  
   goToAreaCalculator = () => this.props.navigation.navigate('AreaCalculator')
-
-  handleCameraType = () => {
-    const { cameraType } = this.state
-
-    this.setState({
-      cameraType:
-        cameraType === Camera.Constants.Type.back
-          ? Camera.Constants.Type.front
-          : Camera.Constants.Type.back
-    })
-  }
 
   takePicture = async () => {
     if (this.camera) {
@@ -38,6 +68,10 @@ class PitchFinder extends Component {
   }
 
   render() {
+    const xValue = this.state.accelerometerData.x
+    const yValue = this.state.accelerometerData.y
+    const pitch = Math.abs((Math.atan2((-xValue), Math.sqrt(yValue*yValue))*57.3)-90)
+
     const { hasPermission } = this.state
     if (hasPermission === null) {
       return <View />;
@@ -46,9 +80,9 @@ class PitchFinder extends Component {
     } else {
       return (
         <View style={{ flex: 1 }}>
-          <Camera style={{ flex: 1 }} type={this.state.cameraType}>
+          <Camera style={{ flex: 1 }} type={this.state.cameraType} ref={ref => {this.camera = ref}}>
             <View style={styles.outputTextBox}>
-              <Text style={styles.outputText}>35</Text>
+              <Text style={styles.outputText}>{pitch.toFixed(0)}</Text>
             </View>
             <View style={styles.iconContainer}>
               <TouchableOpacity
