2c657abe1332b477da23600d7cebaf0c9459f133
jrohan1 <j.rohan1@nuigalway.ie>
2020-03-04 17:58:39 +0000

    Add error checking to manual pitch input

--- screens/ManualPitchEntry.js
+++ screens/ManualPitchEntry.js
@@ -2,19 +2,18 @@ import React, { Component } from 'react';
 import { AsyncStorage, Button, Text, View, TextInput, TouchableOpacity, ScrollView } from 'react-native';
 import { withFirebaseHOC } from '../config/Firebase';
 import { Icon } from 'native-base';
+import ErrorMessage from '../components/ErrorMessage';
 import Header from '../components/Header';
 import { styles } from '../stylesheets/MainStyles';
 import helperFunctions from '../sharedFunctions';
 
 class ManualPitchEntry extends Component {
-  goToPitchFinder = () => this.props.navigation.navigate('PitchFinder');
   goToOrientation = () => this.props.navigation.navigate('Orientation');
-  goToHome = () => this.props.navigation.navigate('Home');
 
   constructor(props) {
     super(props)
     this.state = {
-      area: [],
+      areas: [],
       pitches: []
     }
   }
@@ -24,15 +23,31 @@ class ManualPitchEntry extends Component {
   }
 
   importData = () => {
-    AsyncStorage.getItem('areas').then(value => this.setState({ area: JSON.parse(value) }));
+    AsyncStorage.getItem('areas').then(value => this.setState({ areas: JSON.parse(value) }));
   }
 
   submitInput = () => {
-    helperFunctions.saveArrayData('pitch', this.state.pitches);
+    if (this.state.pitches.length === 0) {
+      this.setState({
+        showError: true
+      });
+    } else if (this.state.areas.length > this.state.pitches.length) {
+      this.setState({
+        showPitchError: true
+      });
+    } else {
+      this.setState({
+        isSubmitted: true
+      }, () => {
+        helperFunctions.saveArrayData('pitch', this.state.pitches);
+        this.goToOrientation();
+      }
+      );
+    }
   }
 
   render() {
-    const pitches = this.state.area.map(
+    const pitches = this.state.areas.map(
       (areas, index) => (
         <View key={index} style={styles.textInput}>
           <TextInput
@@ -44,9 +59,18 @@ class ManualPitchEntry extends Component {
             returnKeyType='done'
             blurOnSubmit
             onChangeText={(pitch) => {
-              let pitches = this.state.pitches;
-              pitches[index] = pitch;
-              this.setState({ pitches })
+              if (pitch > 90) {
+                this.setState({
+                  showPitchToLarge: true
+                });
+              } else {
+                let pitches = this.state.pitches;
+                pitches[index] = pitch;
+                this.setState({
+                  pitches,
+                  showPitchToLarge: false
+                })
+              }
             }}
             value={this.state.pitches[index] ? `${this.state.pitches[index]}` : null}
           />
@@ -57,28 +81,25 @@ class ManualPitchEntry extends Component {
       <View style={styles.container}>
         <ScrollView>
           <Header />
-          <View style={{marginTop: 30, marginBottom: 30}}>
+          <View style={{ marginTop: 30, marginBottom: 30 }}>
             <View style={styles.questionStyle}>
               <Text style={styles.textStyle}>Please enter the pitch of the roof for each area that you have marked.</Text>
               <Text style={styles.subTextStyle}>*Typically between 30 to 40 degrees</Text>
             </View>
             {pitches}
-            {this.state.submitted && (
-              <View>
-                <Icon active type="FontAwesome" name="check" style={styles.checkMarkStyle} />
-              </View>
+            {this.state.showError && !this.state.isSubmitted && (
+              <ErrorMessage errorValue={'*Please input a value for pitch'} />
+            )}
+            {this.state.showPitchError && !this.state.isSubmitted && (
+              <ErrorMessage errorValue={'*Please input a pitch for every area'} />
+            )}
+            {this.state.showPitchToLarge && !this.state.isSubmitted && (
+              <ErrorMessage errorValue={'*Pitch must be less than 90 degrees'} />
             )}
           </View>
-          {!this.state.submitted && (
-            <TouchableOpacity onPress={() => { this.setState({ submitted: true }); this.submitInput() }}>
-            <Text style={styles.nextButton}>Submit</Text>
+          <TouchableOpacity onPress={() => { this.setState({ submitted: true }); this.submitInput() }}>
+            <Text style={styles.nextButton}>Next Step</Text>
           </TouchableOpacity>
-          )}
-          {this.state.submitted && (
-            <TouchableOpacity onPress={this.goToOrientation}>
-              <Text style={styles.nextButton}>Next Step</Text>
-            </TouchableOpacity>
-          )}
         </ScrollView>
       </View>
     )
